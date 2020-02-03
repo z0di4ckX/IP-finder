630cc0b6454e85e3840de2d38aaede452021992e
z0di4ckX <ultimatecracker88@gmail.com>
2020-02-03 16:47:20 -0400

    add new extention

--- src/ip_finder.py
+++ src/ip_finder.py
@@ -3,7 +3,6 @@ import socket
 def run():
     web = input("Entre the website: ")
 
-    scaner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server = web
 
     if web[:7] == "http://":
@@ -12,7 +11,7 @@ def run():
     if web[:8] == "https://":
         web = web[8:]
 
-    lists = ["", "mail.", "ftp."]
+    lists = ["", "mail.", "ftp.", "drive.", "login."]
 
     for x in lists:
         try:
@@ -21,20 +20,5 @@ def run():
             print("[+] " + x + web + " " + ip_resolver + " found.")
         except:
             print("[-] Don't found " + x + web + " " + ip_resolver)
-
-    #testing port
-    def por_scan(port):
-        try:
-            scaner.connect((server, port))
-            return True
-        except:
-            return False
-    
-    for x in range(25):
-        if por_scan(x):
-            print("Port", x, " is opne!")
-        else:
-            print("Port", x, " is closed")
-
 
 run() 
